f22ad14fe75a886dd123d44c9905f4395ab8efda
Valerii Pometun <vmpometun@gmail.com>
2023-03-07 20:11:34 +0200

    task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,24 +3,12 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = (e) => {
-    console.log('played the video!');
-    console.log(e.seconds)
-}
+const onPlay = e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
 
 player.on('timeupdate', onPlay);
 
-// player.getVideoTitle().then(function(title) {
-//      console.log('title:', title);
-// });
-
-
-
-// const player = new Player('handstick', {
-//     id: 19231868,
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.setCurrentTime(currentTime);
