beea5a8b75f7eafb5093de08d72aef2e69a21cdb
Valerii Pometun <vmpometun@gmail.com>
2023-03-08 00:45:16 +0200

    task #3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,24 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = e => {
-  localStorage.setItem('videoplayer-current-time', e.seconds);
+const onPlay = event => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
 };
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.on('timeupdate', onPlay);
-
-player.setCurrentTime(currentTime);
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+// const textareaRef = document.querySelector('textarea');
+
+formRef.addEventListener('input', handleFormInput);
+formRef.addEventListener('submit', handleFormSubmit);
+
+populateInputFiels();
+const feedbackFormData = {};
+
+function handleFormInput(event) {
+  feedbackFormData[event.target.name] = event.target.value;
+  const stringifyFormData = JSON.stringify(feedbackFormData);
+  localStorage.setItem('feedback-form-state', stringifyFormData);
+  // ----------------------------------------------------------------------------
+  //   const inputValue = event.target.value;
+
+  //   localStorage.setItem('feedback-form-state', inputValue);
+
+  //   const savedfeedback = localStorage.getItem('feedback-form-state');
+  //   const parsedfeedback = JSON.parse(savedfeedback);
+
+  //   const parsedfeedback = localStorage.getItem('feedback-form-state');
+  //   event.target.textContent = parsedfeedback;
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem('feedback');
+}
+
+function populateInputFiels() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  const parsedSavedMessage = JSON.parse(savedMessage);
+  console.log(parsedSavedMessage);
+  console.dir(formRef.elements);
+}
