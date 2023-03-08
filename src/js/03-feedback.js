c935f98c1468929bdd1e71ca1e47b361a0a55339
Valerii Pometun <vmpometun@gmail.com>
2023-03-08 11:44:19 +0200

    task #3 almoust done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,31 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
-// const textareaRef = document.querySelector('textarea');
 
-formRef.addEventListener('input', handleFormInput);
+formRef.addEventListener('input', throttle(handleFormInput, 500));
 formRef.addEventListener('submit', handleFormSubmit);
 
 populateInputFiels();
-const feedbackFormData = {};
+
+const formData = {};
 
 function handleFormInput(event) {
-  feedbackFormData[event.target.name] = event.target.value;
-  const stringifyFormData = JSON.stringify(feedbackFormData);
+  formData[event.target.name] = event.target.value;
+  const stringifyFormData = JSON.stringify(formData);
   localStorage.setItem('feedback-form-state', stringifyFormData);
-  // ----------------------------------------------------------------------------
-  //   const inputValue = event.target.value;
-
-  //   localStorage.setItem('feedback-form-state', inputValue);
-
-  //   const savedfeedback = localStorage.getItem('feedback-form-state');
-  //   const parsedfeedback = JSON.parse(savedfeedback);
-
-  //   const parsedfeedback = localStorage.getItem('feedback-form-state');
-  //   event.target.textContent = parsedfeedback;
 }
 
 function handleFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
-  localStorage.removeItem('feedback');
 }
 
 function populateInputFiels() {
-  const savedMessage = localStorage.getItem('feedback-form-state');
-  const parsedSavedMessage = JSON.parse(savedMessage);
-  console.log(parsedSavedMessage);
-  console.dir(formRef.elements);
-}
+  const savedFormData = localStorage.getItem('feedback-form-state');
+  const parsedFormData = JSON.parse(savedFormData);
+  if (parsedFormData) {
+    formRef.elements.message.value = parsedFormData.message || '';
+    formRef.elements.email.value = parsedFormData.email || '';
+  }
+} 
